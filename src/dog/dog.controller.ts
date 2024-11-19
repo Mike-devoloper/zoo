03c8b61efa05460b9b126427a29f125e4b49b36a
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2024-11-20 07:27:29 +0900

    BRR: initial setup

---
+++ src/dog/dog.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Param, Query, Req, Res} from '@nestjs/common';
+import { DogService } from './dog.service';
+import { Request, Response } from 'express';
+
+@Controller('dog')
+export class DogController {
+    constructor(private readonly dogService: DogService) {}
+
+    @Get()
+    public getHello(): string {
+        return this.dogService.getHello()
+    }
+
+    @Get("greet/:id") 
+    public introduce(/*@Req() request:Request, @Res() response: Response*/ @Param('id') id: string, @Query() query: any ) {
+        console.log("Prams", id)
+        console.log("Query", query)
+        return this.dogService.introduce()
+        // response.status(200).send(result);
+    }
+}
---
+++ src/dog/dog.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class DogService {
+
+
+    public getHello(): string {
+        return "Woof Woof"
+    }
+
+    public introduce():string {
+        return "My name is Rex"
+    }
+}
